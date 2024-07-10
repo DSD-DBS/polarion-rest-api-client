3f6340179c805080dea1d91502b5e67d19836cc0
Michael Harbarth <michael.harbarth@deutschebahn.com>
2024-07-10 09:03:50 +0200

    feat: don't retry on 404 errors

--- tests/test_client_error_responses.py
+++ tests/test_client_error_responses.py
@@ -25,9 +25,27 @@ def test_faulty_error_message(
         )
 
     e = e_info.value
-    assert len(e.args) == 5
-    assert e.args[0][0] == "400"
+    assert len(e.args) == 6
+    assert len(httpx_mock.get_requests()) == 2
+    assert e.args[1][0] == "400"
     assert (
-        e.args[0][1] == "Unexpected token, BEGIN_ARRAY expected, but was : "
+        e.args[1][1] == "Unexpected token, BEGIN_ARRAY expected, but was : "
         "BEGIN_OBJECT (at $.data)"
     )
+
+
+def test_dont_retry_on_404(
+    client: polarion_api.OpenAPIPolarionProjectClient,
+    httpx_mock: pytest_httpx.HTTPXMock,
+):
+    with open(TEST_FAULTS_ERROR_RESPONSES, encoding="utf8") as f:
+        httpx_mock.add_response(404, json=json.load(f))
+
+    with pytest.raises(polarion_api.PolarionApiException) as e_info:
+        client.get_document(
+            "MySpaceId", "MyDocumentName", {"fields[documents]": "@all"}
+        )
+
+    e = e_info.value
+    assert len(httpx_mock.get_requests()) == 1
+    assert e.args[0] == 404
