dea96561b5702cde48fb1e3f28cc9459b3d4657e
Michael Harbarth <michael.harbarth@deutschebahn.com>
2025-04-16 13:21:55 +0200

    fix: work item approvals type in the spec

--- polarion_rest_api_client/open_api_client/models/workitems_single_get_response_data_relationships_approvals_data_item_type.py
+++ polarion_rest_api_client/open_api_client/models/workitems_single_get_response_data_relationships_approvals_data_item_type.py
@@ -7,7 +7,7 @@ from enum import Enum
 class WorkitemsSingleGetResponseDataRelationshipsApprovalsDataItemType(
     str, Enum
 ):
-    WORKRECORDS = "workrecords"
+    WORKITEM_APPROVALS = "workitem_approvals"
 
     def __str__(self) -> str:
         return str(self.value)
